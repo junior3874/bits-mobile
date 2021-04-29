f2ba928f4a6903d5a6ece941cbe0e2f8908b9be2
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-28 21:31:14 -0300

    add customized success toast

--- src/lib/toastConfig.js
+++ src/lib/toastConfig.js
@@ -21,6 +21,26 @@ const toastConfig = {
       text2={text2}
     />
   ),
+
+  success: ({ text1, text2, ...rest }) => (
+    <BaseToast
+      {...rest}
+      style={{ borderLeftColor: "green" }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 18,
+        fontWeight: "500",
+        paddingTop: 7,
+      }}
+      text2Style={{
+        fontSize: 15,
+        color: "#666",
+        paddingBottom: 7,
+      }}
+      text1={text1}
+      text2={text2}
+    />
+  ),
 };
 
 export default toastConfig;
