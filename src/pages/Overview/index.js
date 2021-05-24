801c754d0ffcc5f8d267244d6e6e92a4495e84e7
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-05-24 16:28:47 -0300

    get summary from the api

--- src/pages/Overview/index.js
+++ src/pages/Overview/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigation, CommonActions } from "@react-navigation/native";
 import { View } from "react-native";
 import { Feather } from "@expo/vector-icons";
@@ -38,35 +38,66 @@ import {
 import BottomDivider from "../../components/BottomDivider";
 import Chart from "../../components/Chart";
 
+import { formatBalance } from "../../utils/formatBalance";
+
 function Overview() {
+  const [walletSummary, setWalletSummary] = useState({
+    expenses: 0,
+    incomes: 0,
+    balance: 0,
+  });
   const { username } = useContext(AuthContext);
   const navigation = useNavigation();
 
   useEffect(() => {
-    (async () => {
-      const response = await api
-        .get("/wallet")
-        .then(res => ({ error: false, data: res.data }))
-        .catch(err => ({ error: true, err }));
-
-      if (response.error) {
-        navigation.dispatch(
-          CommonActions.reset({
-            index: 0,
-            routes: [
-              {
-                name: "CreateWallet",
-                params: {
-                  title: "Vamos criar sua primeira carteira!",
-                  canGoBack: true,
-                },
-              },
-            ],
-          })
-        );
-      }
-    })();
+    redirectUserIfNoWalletIsFound();
+    fetchWalletSummary();
   }, []);
+
+  async function redirectUserIfNoWalletIsFound() {
+    const response = await api
+      .get("/wallet")
+      .then(res => ({ error: false, data: res.data }))
+      .catch(err => ({ error: true, err }));
+
+    if (response.error) {
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [
+            {
+              name: "CreateWallet",
+              params: {
+                title: "Vamos criar sua primeira carteira!",
+                canGoBack: true,
+              },
+            },
+          ],
+        })
+      );
+    }
+  }
+
+  async function fetchWalletSummary() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+
+    const response = await api
+      .get(`/transactions/index/month?year=${year}&month=${month}`)
+      .then(res => ({ error: false, data: res.data }))
+      .catch(err => ({ error: true, err }));
+
+    if (!response.error) {
+      const { expenses, incomes } = response.data.expensesAndIncome;
+
+      setWalletSummary({
+        expenses: expenses * 100,
+        incomes: incomes * 100,
+        balance: 0, // TODO
+      });
+    }
+  }
 
   function navegatePage(pageName) {
     return navigation.navigate(pageName);
@@ -115,19 +146,25 @@ function Overview() {
             <InfoCardItem first>
               <Feather name="plus-square" size={20} color="#8900f2" />
               <InfoCardItemTitle>Receitas</InfoCardItemTitle>
-              <InfoCardItemValue>R$ 2.148,23</InfoCardItemValue>
+              <InfoCardItemValue>
+                {formatBalance(walletSummary.incomes, "R$")}
+              </InfoCardItemValue>
             </InfoCardItem>
 
             <InfoCardItem>
               <Feather name="minus-square" size={20} color="#8900f2" />
               <InfoCardItemTitle>Despesas</InfoCardItemTitle>
-              <InfoCardItemValue>R$ 1.321,47</InfoCardItemValue>
+              <InfoCardItemValue>
+                {formatBalance(walletSummary.expenses, "R$")}
+              </InfoCardItemValue>
             </InfoCardItem>
 
             <InfoCardItem last>
               <Feather name="dollar-sign" size={20} color="#8900f2" />
               <InfoCardItemTitle>Saldo</InfoCardItemTitle>
-              <InfoCardItemValue bold>R$ 826,76</InfoCardItemValue>
+              <InfoCardItemValue bold>
+                {formatBalance(walletSummary.balance, "R$")}
+              </InfoCardItemValue>
             </InfoCardItem>
           </InfoCard>
         </Info>
