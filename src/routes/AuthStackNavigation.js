bcfa34a162e4445196c6a50658a438e4bca99c43
Wellington Rufino Jr <63420865+junior3874@users.noreply.github.com>
2021-05-24 22:52:40 -0300

    Update AuthStackNavigation.js

--- src/routes/AuthStackNavigation.js
+++ src/routes/AuthStackNavigation.js
@@ -6,7 +6,6 @@ import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 
-
 const Stack = createStackNavigator();
 
 function AuthStackNavigation() {
