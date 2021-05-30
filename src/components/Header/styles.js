b03db6c89959344ea58c16395e78348ee768b989
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-05-30 18:32:10 -0300

    create Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 162px;
+  background: #8900f2;
+  padding: 16px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 25px;
+  position: absolute;
+  bottom: 16px;
+  left: 16px;
+`;
